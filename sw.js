b4bcac26161bbbc54cd12fd3433b7565e6f6588f
jermowery <jermowery@gmail.com>
2020-08-23 21:40:44 -0700

    update version to invalidate old cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            
   '/',                     
